02c3f3d06b952c6c90232209b38124cd30f40e9e
Hans Buss <hans.buss@mailfence.com>
2026-01-14 15:52:37 -0300

    funcional

---
+++ cmd/reading-app/main.go
@@ -0,0 +1,17 @@
+/*
+Reading App.
+
+Authors: Brenda & Hans.
+
+main.go
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+
+	b := book{"the stand", "stephen king", "9780385121682", "1", "en-us", "", 9.5, "1-1-2026", "30-1-2026", []string{"1-1-2026", "2-1-2026"}}
+	fmt.Println(b.String())
+}
