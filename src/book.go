94cf4c00487cd0b42a7f1b3c955849852535191e
Brenda-Machado <araucaria2011@gmail.com>
2026-01-14 15:48:47 -0300

    fix format and type issues

--- src/book.go
+++ src/book.go
@@ -6,7 +6,7 @@ Authors: Brenda & Hans.
 book.go
 */
 
-package main //lalal =)
+package main
 
 import (
 	"fmt"
@@ -15,23 +15,23 @@ import (
 type book struct {
 	name, author, isbn, edition, language, review string
 	rating                                        float64
-	startDate, endDate                          string // To-Do: change to d/m/y date type
-	daysRead                                     []string
+	startDate, endDate                            string // To-Do: change to d/m/y date type
+	daysRead                                      []string
 }
 
 //////////////////func (b* book) se
 
 func (b book) String() string {
 	return fmt.Sprintf("Name: %s\n"+
-			    Author: %s\n
-		    	    ISBN: %s\n
-		    	    edition %s\n
-		    	    language: %s\n
-		    	    review: %s\n
-		    	    rating: %f\n
-		    	    startDate: %s\n
-		    	    endDate: %s\n
-		    	    DaysRead: %d\n",
-			    b.name, b.author, b.isbn, b.edition, b.language, b.review, b.rating, b.startDate, b.endDate, len(b.daysRead))
-			    
+		"Author: %s\n"+
+		"ISBN: %s\n"+
+		"edition %s\n"+
+		"language: %s\n"+
+		"review: %s\n"+
+		"rating: %f\n"+
+		"startDate: %s\n"+
+		"endDate: %s\n"+
+		"DaysRead: %d\n",
+		b.name, b.author, b.isbn, b.edition, b.language, b.review, b.rating, b.startDate, b.endDate, len(b.daysRead))
+
 }
